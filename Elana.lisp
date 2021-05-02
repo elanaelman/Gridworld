0afc9e05ccc0a964654ee7c09dfb835b7af63c8a
Elana Elman <elana.elman@gmail.com>
2021-05-02 16:59:13 -0400

    Actions and supporting objects and functions for buy, smell, read, and say. Still need to add listen, but that's not relevant for the single agent world.

--- Elana.lisp
+++ Elana.lisp
@@ -1,62 +1,68 @@
 ; Elana's gridworld functions.
 
-(def-roadmap '(me-home boss-home office) '((path1 me-home 2 office) (path2 boss-home 3 office)))
-
-
 ;objects:
 
-(def-object 'person '(can_walk can_talk can_give can_take is_animate))
+(def-object 'person '(can_talk is_animate))
+(def-object 'note '(is_readable))
 
-(place-object 'ME 'person 'me-home 0  
+(place-object 'AG 'person 'me-home 0  
     nil ;no associated things
 	'(
 	  ;self-knowledge
-	  (is_tired_to_degree ME 0)
-	  (is_hungry_to_degree ME 0)
-	  (has_money ME 100)
-	  (has_age ME 25) 
-	  (has_name ME Alex)
-	  (has_job ME employee)
-	  (works_at ME office)
-	  (is_single ME)
+	  (is_tired_to_degree AG 0)
+	  (is_hungry_to_degree AG 0)
+	  (has_money AG 100)
+	  (has_age AG 25) 
+	  (has_name AG Alex)
+	  (has_job AG employee)
+	  (works_at AG office)
+	  (is_single AG)
 	  ;other knowledge
-	  (has_name Boss Carol) (has_job Boss employer) (works_at Boss office)
+
+	  ;depends on Boss object:
+	  ;(has_name Boss Carol) (has_job Boss employer) (works_at Boss office)
 	 )
 	 nil ;propositional attitudes?
 	 )
 
-(place-object 'Boss 'person 'boss-home 0
-	nil
-	'(
-	  ;self-knowledge
-	  (is_tired_to_degree Boss 0)
-	  (is_hungry_to_degree Boss 0)
-	  (has_money Boss 200)
-	  (has_age Boss 30)
-	  (has_name Boss Carol)
-	  (has_job Boss employer)
-	  (works_at Boss office)
-	  (is_single Boss)
-      ;other knowledge
-	  (has_name ME Alex)
-	  (has_job ME employee)
-	  (works_at ME office)
-	)
-	nil)
+;(place-object 'Boss 'person 'boss-home 0
+;	nil
+;	'(
+;	  ;self-knowledge
+;	  (is_tired_to_degree Boss 0)
+;	  (is_hungry_to_degree Boss 0)
+;	  (has_money Boss 200)
+;	  (has_age Boss 30)
+;	  (has_name Boss Carol)
+;	  (has_job Boss employer)
+;	  (works_at Boss office)
+;	  (is_single Boss)
+;      ;other knowledge
+;	  (has_name AG Alex)
+;	  (has_job AG employee)
+;	  (works_at AG office)
+;	)
+;	nil)
 
 
 ;actions:
 
+;TODO: test. Assuming format from lisp 2 project, might need an extra car.
+(defun has_money? (ag)
+	(let ((ans (answer_to_whq? (has_money ag ?x))))
+		(cond (equal (car ans) 'not) -1 (caddr ans))))
+
 (setq buy
 	  (make-op
 		:name 'buy
-		:pars '(?item ?store ?cost)
-		:preconds '((is_at AG ?store) 
-					(sells ?store ?item ?cost) ;need sells? check property?
-					(has_money AG ?cost))
-		:effects '((has_money AG (- (money? AG) ?cost) (owns AG ?item))) ;need owns, money?
-		:time-required '1
-		:value '0 ;value of item? idk
+		:pars '(?item ?cost)
+		:preconds '((is_at AG supermarket) 
+					(sells supermarket ?item ?cost)	
+					; ^ Add knows that ... and person to ask prices
+					(>= (has_money? AG) ?cost))
+		:effects '((has_money AG (- (has_money? AG) ?cost) (has AG ?item)))
+		:time-required 1
+		:value ?cost
 	  )
 )
 
@@ -65,24 +71,22 @@
 		:name 'buy.actual
 		:pars '(?item ?store ?cost)
 		:startconds '((is_at AG ?store) 
-					  (sells ?store ?item ?cost) ;need sells? check property?
-					  (has_money AG ?cost))
+					  (sells ?store ?item ?cost)
+					  (>= (has_money? AG) ?cost))
 		:stopconds nil
-		:deletes nil
-		:adds '((has_money AG (- (money? AG) ?cost)) (owns AG ?item))
-
-		;elapsed time?
+		:deletes '((has_money AG (has_money? AG)))
+		:adds '((has_money AG (- (has_money? AG) ?cost)) (has AG ?item))
 	  )
 )
 
 (setq smell 
 	  (make-op 
-		:name 'sleep
+		:name 'smell
 		:pars '(?item)
-		:preconds '((owns AG ?item) (is_food ?item))
-		:effects '((knows AG (whether (is_expired ?item))))
+		:preconds '((has AG ?item) (is_food ?item))
+		:effects '((knows AG (whether (is_expired ?item)))) ;TODO: check this
 		:time-required '1
-		:value '(if (is_expired ?item) '1 '-1)
+		:value '(if (expired? ?item) 0.5 -0.5) ;TODO: expired?
 	  )
 )
 
@@ -90,45 +94,69 @@
 	  (make-op.actual
 		:name 'smell.actual
 		:pars '(?item)
-		:startconds '((owns AG ?item) (is_food ?item))
+		:startconds '((has AG ?item) (is_food ?item))
 		:stopconds nil
 		:deletes nil
 		:adds '((knows AG (whether (is_expired ?item))))
 	  )
 )
 
+;TODO: test, again assuming notation from lisp2 assignment.
+;Also, might need to add case for no message found.
+(defun message_of? (n)
+	(caddr (answer_to_whq? (has_message_that n ?message))))
+
 (setq read
 	  (make-op
 		:name 'read
 		:pars '(?item ?location)
-		:preconds '((is_at AG ?location) (is_at ?item ?location))
-		:effects '(knows AG (message_in ?item))
-		:time-required '(* 0.5 (length_of (message_in ?item)))
-		:value '0
+		:preconds '((is_at AG ?location) (is_at ?item ?location) (is_readable ?item))
+		:effects '(knows AG (message_of? ?item))	;TODO: message_of?
+		:time-required 3
+		:value 0
 	  )
 )
+
 
 (setq read.actual
 	  (make-op.actual
 		:name 'read.actual
 		:pars '(?item ?location)
-		:startconds '((is_at AG ?location) (is_at ?item ?location))
+		:startconds '((is_at AG ?location) (is_at ?item ?location) (is_readable ?item))
 		:stopconds '((there_is_a_fire))
 		:deletes nil
-		:adds '(knows AG (message_in ?item))
+		:adds '(knows AG (message_of? ?item))
 	  )
 )
+
+;Example of a readable item
+(place-object 'note1 'note 'home 0 nil '((has_message_that note1 (wants Alice apple1))) nil)
+
 
 (setq say
 	  (make-op
 		:name 'say
 		:pars '(?message ?location)
-		:preconds (is_at AG ?location)
-		:effects '((said AG ?message ?location (current_time))) ;time?
-		:time-required '(*0.5 (length_of ?message))
-		:value '0
+		:preconds '((is_at AG ?location))
+		:effects '((said AG ?message ?location (current_time?)))
+		:time-required 2
+		:value 0
 	  )
 )
+
+(setq say.actual
+		(make-op
+		  :name 'say.actual
+		  :pars '(?message ?location)
+		  :startconds '((is_at AG ?location))
+		  :stopconds '((there_is_a_fire))
+		  :deletes nil
+		  :adds '((said AG ?message ?location (current_time?)))
+		)
+)
+
+
+
 
 ;setup:
 
