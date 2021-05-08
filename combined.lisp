89fb5726943df4e5c44aa96ede55032bf8311b47
Elana <eelman2@u.rochester.edu>
2021-05-08 11:31:20 -0400

    first functional version in combined.lisp

--- combined.lisp
+++ combined.lisp
@@ -32,13 +32,14 @@
 (def-object 'burger '(is_edible (has_cost 6)))
 (def-object 'water '(is_potable (has_cost 1)))
 
-(place-object 'chicken1 'chicken 'home 0 nil nil nil)
+(place-object 'chicken1 'chicken 'home 0 nil '((not (is_expired chicken1))) nil)
+(place-object 'chicken2 'chicken 'home 0 nil '((is_expired chicken2)) nil)
 (place-object 'water1 'water 'home 0 nil nil nil)
 ;(place-object 'gridworld_supermarket 'supermarket 'supermarket 0 '((chicken chicken1)) nil nil)
 
 ;; agent
 (place-object 'AG 'person 'home 0
-	nil
+	'((chicken chicken1) (chicken chicken2))
 	'(
 	(is_tired_to_degree AG 0)
 	(is_hungry_to_degree AG 5) ;set it to be hungry
@@ -77,10 +78,15 @@
 				 (is_at AG ?y) 
 				 (is_at ?x ?y) 
 				 (is_edible ?x) 
+				 (not (is_expired ?x))
+				 (knows AG (whether (is_expired ?x)))
 				 (knows AG (whether (is_edible ?x)))
+				 (has AG ?x)
 			    )
 	:effects '( (is_hungry_to_degree AG 0.0) 
-				(not (is_hungry_to_degree AG ?h)) )
+				(not (is_hungry_to_degree AG ?h)) 
+				(not (has AG ?x))
+				(not (is_at ?x ?y)))
 	:time-required 1
 	:value '(* 2 ?h)
 	)
@@ -94,11 +100,13 @@
 				   (is_at AG ?y) 
 				   (is_at ?x ?y) 
 				   (is_edible ?x) 
+				   (has AG ?x)
 				   (knows AG (whether (is_edible ?x))) )
 	:stopconds '( (there_is_a_fire)
 				  (there_is_a_flood) 
 				  (is_hungry_to_degree AG 0.0) )
-	:deletes '( (is_hungry_to_degree AG ?#1) )
+	:deletes '( (is_hungry_to_degree AG ?#1) 
+			    (has AG ?x) (is_at ?x ?y))
 	:adds '( (is_hungry_to_degree AG 0.0) )
 	)
 )
@@ -212,55 +220,164 @@
     )
 )
 
-;;model of drive
-(setq drive 
-	(make-op :name 'drive :pars '(?x ?y ?z ?f ?c ?m)
-	:preconds '((is_at AG ?x)        
-				(is_on ?x ?z)        
-				(is_on ?y ?z) (point ?y)
-				(navigable ?z)
-                (is_tired_to_degree AG ?f) 
-                (has_car AG ?c)
-                (has_gas ?c))
-    :effects '((is_at AG ?y) 
-    		   (not (is_at AG ?x))
-               ;(is_tired_to_degree AG (+ ?f 0.5))
-               (is_tired_to_degree AG (+ ?f (* 0.1 (distance_from+to+on? ?x ?y ?z))));less tired than walking
-               (not (is_tired_to_degree AG ?f)) 
-               (mileage_left (- ?m (distance_from+to+on? ?x ?y ?z))))
-    :time-required '(/ (distance_from+to+on? ?x ?y ?z) 10) ;you take 1/10 of the time to drive
-    :value '(- 0.5 ?f) ;less fatiguing than walking
-    )
+(setq smell 
+	  (make-op 
+		:name 'smell
+		:pars '(?item)
+		:preconds '((has AG ?item) (is_edible ?item))
+		:effects '((knows AG (whether (is_expired ?item))))
+		:time-required '1
+		:value 1 
+	  )
 )
 
-;;actual drive
-(setq drive.actual 
-	(make-op.actual :name 'drive.actual :pars '(?x ?y ?z ?f ?c ?m)
-	:startconds '((is_at AG ?x)        
-				  (is_on ?x ?z)        
-				  (is_on ?y ?z) (point y)
-				  (navigable ?z)
-                  (is_tired_to_degree AG ?f) 
-                  (has_car AG ?c)
-                  (has_gas ?c)
-                 )
-    :stopconds '((not (navigable ?z)) 
-    			 (is_at AG ?y) )
-    :deletes '((is_at AG ?#1)
-    		   (is_tired_to_degree AG ?#2))
-    :adds '((is_at AG (the_pt+units_from+towards+on_road? (* 0.1 (elapsed_time?)) ?x ?y ?z))
-    		(is_at AG (the_pt+units_from+towards+on_road? (- (distance_from+to+on? ?x ?y ?z) (* 1 (elapsed_time?))) ?y ?x ?z))
-    	    (is_on (the_pt+units_from+towards+on_road? (* 0.1 (elapsed_time?)) ?x ?y ?z) ?z)
-    	    (is_on (the_pt+units_from+towards+on_road? (- (distance_from+to+on? ?x ?y ?z) (* 1 (elapsed_time?))) ?y ?x ?z) ?z)
-    		(is_tired_to_degree AG (+ ?f (* 0.1 (elapsed_time?)))) 
-    		(mileage_left (- ?m (distance_from+to+on? ?x ?y ?z)))
-    		)
-    )
+(setq smell.actual
+	  (make-op.actual
+		:name 'smell.actual
+		:pars '(?item)
+		:startconds '((has AG ?item) (is_edible ?item))
+		:stopconds '((knows AG (whether (is_expired ?item))))
+		:deletes nil
+		:adds '((knows AG (whether (is_expired ?item))))
+	  )
 )
 
 
 
+;might need to add case for no message found.
+(defun message_of? (n)
+	(caddar (answer_to_whq? (list 'has_message_that n '?message))))
+
+(setq read
+	  (make-op
+		:name 'read
+		:pars '(?item ?location)
+		:preconds '((is_at AG ?location) (is_at ?item ?location) (is_readable ?item))
+		:effects '((knows AG (message_of? ?item)))
+		:time-required 3
+		:value 3
+	  )
+)
+
+
+(setq read.actual
+	  (make-op.actual
+		:name 'read.actual
+		:pars '(?item ?location)
+		:startconds '((is_at AG ?location) (is_at ?item ?location) (is_readable ?item))
+		:stopconds '((there_is_a_fire) (knows AG (message_of? ?item)))
+		:deletes nil
+		:adds '((knows AG (message_of? ?item)))
+	  )
+)
+
+;Example of a readable item
+(place-object 'note1 'note 'home 0 nil '((has_message_that note1 (is_single Carol))) nil)
+
+(defun answer_to_ynq? (wff)
+	(check-yn-fact-in-kb 'NIL wff (state-node-wff-htable *curr-state-node*))
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Function answer_to_ynq.actual? returns a well-formed formula indicating  
+;; whether the arg wff is currently in AG's KB, under the closed world 
+;; assumption. In addition, the answer is translated into a proper English 
+;; sentence and printed on screen.  For example, if AG is currently hungry 
+;; according to AG's KB, then (is_hungry AG) is returned as the response to 
+;; (answer_to_ynq.actual? '(is_hungry AG)), and ``AG is hungry'' without the 
+;; double quotes is printed.  Otherwise, (not (is_hungry AG)) is 
+;; returned and ``it is not the case that AG is hungry'' is printed without 
+;; the double quotes.
+;; This is the `actual' version.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun answer_to_ynq.actual? (wff)
+	(check-yn-fact-in-kb 'T wff (state-node-wff-htable *curr-state-node*))
+)
+
+
+(defun answer_to_whq? (wff)
+	(check-whq-answer-in-kb 'NIL wff (state-node-wff-htable *curr-state-node*))
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Function answer_to_whq.actual? returns a collection of well-formed 
+;; formula(s) as the answer to the arg wff reflecting what are currently in 
+;; AG's KB, under the closed world assumption. Arg wff is a wh-question 
+;; with variables prefixed with ? appearing in slots filled by wh-words.  
+;; For example, if AG likes only APPLE1 and BANANA2 according to AG's KB,
+;; ((likes AG APPLE1) (likes AG BANANA2)) is returned as the response to 
+;; (answer_to_whq.actual? '(likes AG ?wh)), and ``AG likes APPLE1'' and ``AG likes 
+;; BANANA2'' without double quotes are printed on two lines.  If no answer 
+;; is found, '(not (knows (AG the-answer))) is returned and ``it is not the 
+;; case that AG knows the answer'' without the double quotes is printed .
+;; This is the `actual' version.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun answer_to_whq.actual? (wff)
+	(check-whq-answer-in-kb 'T wff (state-node-wff-htable *curr-state-node*))
+)
+(setq answer_user_ynq 
+      (make-op :name 'answer_user_ynq :pars '(?q)
+        :preconds '( (wants USER (that (tells AG USER (whether ?q)))) )
+        :effects '( (not (wants USER (that (tells AG USER (whether ?q)))))
+                    (knows USER (that (answer_to_ynq? ?q)))
+			  		)
+        :time-required 1
+        :value 10
+	)
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; With operator answer_user_ynq.actual, AG answers the yes-no question 
+;; ?q asked by USER.
+;; This is the `actual' version.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq answer_user_ynq.actual 
+	(make-op.actual :name 'answer_user_ynq.actual :pars '(?q)
+	:startconds '( (wants USER (that (tells AG USER (whether ?q)))) )
+	:stopconds '( (not (wants USER (that (tells AG USER (whether ?q))))) )
+	:deletes '( (wants USER (that (tells AG USER (whether ?q)))) )
+	:adds '( ;(knows USER (that (answer_to_ynq?.actual ?q)))				
+					 (says+to+at_time AG (that (answer_to_ynq.actual? ?q)) USER (current_time?))
+					 (not (wants USER (that (tells AG USER (whether ?q)))))
+		   	 )
+	)
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; With operator answer_user_whq, AG answers the wh-question ?q asked by 
+
+(setq answer_user_whq 
+	(make-op :name 'answer_user_whq :pars '(?q)
+	:preconds '( (wants USER (that (tells AG USER (answer_to_whq ?q)))) )
+	:effects '( (not (wants USER (that (tells AG USER (answer_to_whq ?q)))))
+				(knows USER (that (answer_to_whq? ?q)))
+			  )
+	:time-required 1
+	:value 10
+	)
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; With operator answer_user_whq.actual, AG answers the wh-question ?q 
+;; asked by USER.
+;; This is the `actual' version.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq answer_user_whq.actual 
+	(make-op.actual :name 'answer_user_whq.actual :pars '(?q)
+	:startconds '( (wants USER (that (tells AG USER (answer_to_whq ?q)))) )
+	:stopconds '( (not (wants USER (that (tells AG USER (answer_to_whq ?q))))) )
+	:deletes '( (wants USER (that (tells AG USER (answer_to_whq ?q)))) )
+	:adds	'( ;(knows USER (that (answer_to_whq.actual? ?q)))				
+			   (says+to+at_time AG (that (answer_to_whq.actual? ?q)) USER (current_time?))
+			   (not (wants USER (that (tells AG USER (answer_to_whq ?q)))))
+			 )
+	)	
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; With operator walk, AG walks from point ?x to point ?y on road ?z, with 
 ;setup:
-(setq *operators* '(eat drink walk drive))
+(setq *operators* '(eat drink walk smell read answer_user_ynq answer_user_whq))
 
-(setq *search-beam* (list (cons 1 *operators*)))
+(setq *search-beam* (list (cons 2 *operators*) (cons 2 *operators*) (cons 2 *operators*)))
+
--- my-init.lisp
+++ my-init.lisp
@@ -8,5 +8,5 @@
 (load "go.lisp")
 (load "implement-effects.lisp")
 (load "simulation-and-function-evaluation.lisp")
-(load "Elana.lisp")
+(load "combined")
 (initialize-state-node)
